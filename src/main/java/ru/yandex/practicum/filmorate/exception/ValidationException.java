176e8f0cc86fadd1426b6687a1c6772a32438948
Denis <schishckin.dennis@yandex.ru>
2023-08-11 19:28:42 +0400

    добавил пробелы перед еще в одном месте {

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class ValidationException extends Exception{ // возможно ошибку валидации надо отдельно для каждого нюанса писать
+public class ValidationException extends Exception {
     public ValidationException(String massage) {
         super(massage);
     }
