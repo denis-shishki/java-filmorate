d4d752e2b9bf5bb21116be01837991afcd83c27b
Denis <schishckin.dennis@yandex.ru>
2023-09-27 00:14:40 +0400

    отредактировал содержимое файла data.sql для корректного прохождения тестов

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,21 +15,4 @@ SELECT 'R' UNION ALL
 SELECT 'NC-17'
 WHERE NOT EXISTS (SELECT * FROM PUBLIC.MPA);
 
-INSERT INTO PUBLIC.USERS (name, login, email, birthday)
-VALUES ('Имя','Логин','почта@точка.ру','2000-05-05'),
-       ('Имя2','Логин2','почта2@точка.ру','1999-05-05'),
-       ('Имя3','Логин3','почта3@точка.ру','1998-05-05'),
-       ('Имя4','Логин4','почта4@точка.ру','1997-05-05');
-
-
-INSERT INTO PUBLIC.FILMS (name, description, release_date, duration, mpa_id)
-VALUES ('Фильм', 'Описание фильма','2000-10-10', 120, 3),
-       ('Фильм2', 'Описание фильма2','2000-11-10', 200, 1),
-       ('Фильм3', 'Описание фильма3','2000-12-10', 60, 2);
-
-INSERT INTO PUBLIC.FRIENDS_LIST (user_id, friend_id, confirmation)
-VALUES (1, 2, 't'),
-       (2, 1, 't'),
-       (2, 3, 'f');
-
 
